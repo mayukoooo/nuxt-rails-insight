d18cec6ca6f94078eadc465b4c057e82f4dac021
mayukoooo <yamagishi.work3@gmail.com>
2022-04-06 22:56:20 +0900

    リンク修正

--- front/components/Footer.vue
+++ front/components/Footer.vue
@@ -5,7 +5,7 @@
       <div class="nav">
         <NuxtLink to="/"><div class="menu"><img src="~/assets/img/works.png" width="70px"></div></NuxtLink>
         <NuxtLink to="/friends"><div class="menu"><img src="~/assets/img/friends.png" width="75px"></div></NuxtLink>
-        <NuxtLink to="/profile/1"><div class="menu"><img src="~/assets/img/profile.png" width="70px"></div></NuxtLink>
+        <NuxtLink to="/profile"><div class="menu"><img src="~/assets/img/profile.png" width="70px"></div></NuxtLink>
       </div>
     </v-footer>
 </div>
