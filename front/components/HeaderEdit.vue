b6d5937bedabcb40195f1d853ee84365f1d7f01b
mayukoooo <yamagishi.work3@gmail.com>
2022-04-10 19:17:34 +0900

    編集アイコン追加

--- front/components/HeaderEdit.vue
+++ front/components/HeaderEdit.vue
@@ -1,11 +1,8 @@
 <template>
   <v-app-bar class="head" fixed app>
-      <NuxtLink to="/works"><img src="~/assets/img/logo.png" width="95px"></NuxtLink>
-      <v-spacer />
-
-      <NuxtLink to=""><p>編集する</p>
-            <!-- <img src="~/assets/img/myworks_icon.png" width="40px"> -->
-      </NuxtLink>
+    <NuxtLink to="/works"><img src="~/assets/img/logo.png" width="95px"></NuxtLink>
+    <v-spacer />
+    <NuxtLink to="/profile/edit"><img src="~/assets/img/pen.png" width="23px"></NuxtLink>
     </v-app-bar> 
 </template>
 
