369377dd04a95bd10ab997c59ef2eb44aa989f05
mayukoooo <yamagishi.work3@gmail.com>
2022-04-17 08:40:31 +0900

    アイコン削除

--- front/components/HeaderMain.vue
+++ front/components/HeaderMain.vue
@@ -1,10 +1,9 @@
 <template>
   <v-app-bar class="head" fixed app>
-      <NuxtLink to="/works"><img src="~/assets/img/logo.png" width="95px"></NuxtLink>
-      <v-spacer />
-
-      <NuxtLink to="/myworks"><img src="~/assets/img/myworks_icon.png" width="45px"></NuxtLink>
-    </v-app-bar> 
+    <NuxtLink to="/works"><img src="~/assets/img/logo.png" width="95px"></NuxtLink>
+    <v-spacer />
+    <!-- <NuxtLink to="/myworks"><img src="~/assets/img/myworks_icon.png" width="45px"></NuxtLink> -->
+  </v-app-bar> 
 </template>
 
 <script>
