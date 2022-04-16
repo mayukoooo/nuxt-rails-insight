e056a5e14a06742b3a25070b9e9b7018901b0809
mayukoooo <yamagishi.work3@gmail.com>
2022-04-17 08:41:48 +0900

    NuxtLinkを動的に修正

--- front/pages/myworks/_categoryid/index.vue
+++ front/pages/myworks/_categoryid/index.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
     <v-app-bar class="head" fixed app>
-    <NuxtLink to="/myworks/1" tag="div">戻る</NuxtLink>
-    <v-spacer />
-    <NuxtLink to="/works"><img src="~/assets/img/logo.png" width="95px"></NuxtLink>
-    <v-spacer />
-    <NuxtLink to="/myworks/categoryid/edit" tag="div"><v-btn class="save" color="#FFAB91">編集</v-btn></NuxtLink>
+      <NuxtLink to="/works" tag="div">戻る</NuxtLink>
+      <v-spacer />
+      <NuxtLink to="/works"
+        ><img src="~/assets/img/logo.png" width="95px"
+      /></NuxtLink>
+      <v-spacer />
+      <NuxtLink :to="'/myworks/' + $route.params.categoryid + '/edit'" tag="div"><v-btn class="save" color="#FFAB91">編集</v-btn></NuxtLink>
     </v-app-bar>
-      <p>my works編集ページ</p>
     <p v-for="answer in answers" :key="answer.id">
       <v-container fluid>
         <v-col cols="12" sm="12">
           {{ answer.question.text }}
-        <v-card class="mx-auto">
-        <v-card-text class="font-weight-medium mt-4 text-left text-subtitle-1">
-          {{ answer.answer }}
-        </v-card-text>
-        </v-card>
+          <v-card class="mx-auto">
+            <v-card-text class="font-weight-medium mt-4 text-left text-subtitle-1">
+              {{ answer.answer }}
+            </v-card-text>
+          </v-card>
         </v-col>
       </v-container>
     </p>
@@ -32,6 +33,7 @@ export default {
   data() {
     return {
       answers: [],
+      params_id: this.$route.params.categoryid,
     };
   },
   computed: {
@@ -39,35 +41,37 @@ export default {
       return this.$store.state.auth.currentUser;
     },
   },
-  async asyncData({ $axios }) {
-    const answers = await $axios.$get(
-      "/v1/answers"
-    );
-    console.log(answers)
+  async asyncData({ $axios, params, store }) {
+    let answers = await $axios.$get("/v1/answers");
+    answers = answers.filter((answer) => {
+      return answer.question.category_id == params.categoryid && answer.user_id == store.state.auth.currentUser.id;
+    });
+    answers.sort((a, b) => {
+      return a.question_id - b.question_id;
+    });
     return { answers };
   },
   methods: {
     async removeAnswer() {
       const res = confirm("本当に削除しますか？");
       if (res) {
-        console.log(this.answers)
-        await Promise.all(this.answers.map((async item => {
-          console.log(item)
-          console.log(item.id)
-          await this.$axios.$delete(`/v1/answers/${item.id}`, {
-            id: item.id
-          });
-        })));
-        this.$router.push("/works")
+        console.log(this.answers);
+        await Promise.all(
+          this.answers.map(async (item) => {
+            console.log(item);
+            console.log(item.id);
+            await this.$axios.$delete(`/v1/answers/${item.id}`, {
+              id: item.id,
+            });
+          })
+        );
+        this.$router.push("/works");
       }
     },
   },
-  fetch({
-    store,
-    redirect
-  }) {
+  fetch({ store, redirect }) {
     store.watch(
-      state => state.auth.currentUser,
+      (state) => state.auth.currentUser,
       (newUser, oldUser) => {
         if (!newUser) {
           return redirect("/login");
@@ -75,7 +79,7 @@ export default {
       }
     );
   },
-}
+};
 </script>
 
 <style>
